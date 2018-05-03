7203745c78eef43324802d5951e31191cbc78bc5
JayJay <jay@vertecx.net>
2018-05-03 12:38:01 -0700

    Removing outline of cloned fruit

--- Assets/Scripts/Controllers/FruitItemController.cs
+++ Assets/Scripts/Controllers/FruitItemController.cs
@@ -33,23 +33,15 @@ public class FruitItemController : Singleton<FruitItemController>
         if (Input.touchCount == 2)
         {
             touch = Input.GetTouch(1);
-            if (touch.phase == TouchPhase.Began)
-            {
-
-            }
-            else if (touch.phase == TouchPhase.Moved ||  touch.phase == TouchPhase.Stationary)
+            if (touch.phase == TouchPhase.Moved ||  touch.phase == TouchPhase.Stationary)
             {
                 selectedFruit.transform.Rotate(touch.deltaPosition.y * SPEED, -touch.deltaPosition.x * SPEED, 0, Space.World);
                 clonedFruit.transform.rotation = selectedFruit.transform.rotation;
             }
-            else
-            {
-
-            }
         }       
     }
 
-    // Controls are enabled only when users finger is touching controls panel
+    // Controls are enabled only when finger is touching controls panel
     public void EnableControls()
     {
         IsControlEnabled = true;
@@ -67,13 +59,14 @@ public class FruitItemController : Singleton<FruitItemController>
         panel_controls.SetActive(true);
         cameraFruitControl.gameObject.SetActive(true);
 
-        // Clone grabbed fruit for render texture
+        // Clone grabbed item (for render texture)
         clonedFruit = Instantiate(selectedFruit.gameObject, new Vector3(-100, -100, -100), selectedFruit.transform.rotation);
+        clonedFruit.GetComponentInChildren<MeshRenderer>().material.SetFloat("_OutlineAlpha", 0f);
         Destroy(clonedFruit.GetComponent<Rigidbody>());
         Destroy(clonedFruit.GetComponent<FruitItem>());
         Destroy(clonedFruit.GetComponent<Collider>());
 
-        // Set render texture camera to look at cloned fruit
+        // Set render texture camera to look at cloned item
         Vector3 camPos = clonedFruit.transform.position;
         camPos.z -= 0.3f;
         cameraFruitControl.transform.position = camPos;
@@ -85,13 +78,16 @@ public class FruitItemController : Singleton<FruitItemController>
         if (!selectedFruit)
             return;
 
+        // Drop item
         selectedFruit.SetGrabbed(false);
         selectedFruit = null;
+
+        // Disable control
         panel_controls.SetActive(false);
         cameraFruitControl.gameObject.SetActive(false);
         cameraFruitControl.transform.SetParent(transform.root.parent);
 
-        // Destroy cloned fruit
+        // Destroy cloned item
         if (clonedFruit)
             Destroy(clonedFruit);
     }    
