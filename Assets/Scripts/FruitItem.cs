89520f4cbf50d6bf91517601eaebea880bde2567
Jay <jay@vertecx.net>
2018-05-08 12:15:30 -0700

    - Increased angular drag - Removed standard shader

--- Assets/Scripts/FruitItem.cs
+++ Assets/Scripts/FruitItem.cs
@@ -37,9 +37,9 @@ public class FruitItem : MonoBehaviour
         material = GetComponentInChildren<MeshRenderer>().material;
         material.CopyPropertiesFromMaterial(new Material(material));
 
-#if UNITY_EDITOR
-        material.shader = Shader.Find("Mobile/Diffuse");
-#endif
+//#if UNITY_EDITOR
+//        material.shader = Shader.Find("Mobile/Diffuse");
+//#endif
     }
 
     void OnEnable()
@@ -88,58 +88,25 @@ public class FruitItem : MonoBehaviour
 
     private void StayWithinBoundaries()
     {
-        // Constaint positions within boundaries (Allow dragging to origin)
-        //if (isGrabbed || !GameController.Instance.IsGameRunning)
-        //{
-            // Offset X
-            float padding = fruitCollider.bounds.size.x / 4f;
-            float offset = roomController.Room.transform.position.x;
-            float x = Mathf.Clamp(transform.position.x, -roomController.MaxDraggableBoundaries.x + padding + offset, roomController.MaxDraggableBoundaries.x - padding + offset);
+        // Offset X
+        float padding = fruitCollider.bounds.size.x / 4f;
+        float offset = roomController.Room.transform.position.x;
+        float x = Mathf.Clamp(transform.position.x, -roomController.MaxDraggableBoundaries.x + padding + offset, roomController.MaxDraggableBoundaries.x - padding + offset);
 
-            // Offset Y
-            float minHeight = fruitCollider.bounds.size.y / 4f;
-            padding = fruitCollider.bounds.size.y / 2f;
-            offset = roomController.Room.transform.position.y;
-            float y = Mathf.Clamp(transform.position.y, minHeight + padding + offset, roomController.MaxDraggableBoundaries.y - padding + offset);
+        // Offset Y
+        float minHeight = fruitCollider.bounds.size.y / 4f;
+        padding = fruitCollider.bounds.size.y / 2f;
+        offset = roomController.Room.transform.position.y;
+        float y = Mathf.Clamp(transform.position.y, minHeight + padding + offset, roomController.MaxDraggableBoundaries.y - padding + offset);
 
-            // Offset Z
-            offset = roomController.Room.transform.position.z;
-            padding = fruitCollider.bounds.size.z / 4f;
-            float z = Mathf.Clamp(transform.position.z, -roomController.MaxDraggableBoundaries.z + padding + offset, roomController.MaxDraggableBoundaries.z - padding + offset);
+        // Offset Z
+        offset = roomController.Room.transform.position.z;
+        padding = fruitCollider.bounds.size.z / 4f;
+        float z = Mathf.Clamp(transform.position.z, -roomController.MaxDraggableBoundaries.z + padding + offset, roomController.MaxDraggableBoundaries.z - padding + offset);
 
-            // Set position
-            transform.position = new Vector3(x, y, z);
-        //}
-        //// Constaint positions within boundaries (Do not allow dragging to origin)
-        //else
-        //{
-        //    // Offset X
-        //    float padding = fruitCollider.bounds.size.x / 4f;
-        //    float x;
-        //    float offset = roomController.Room.transform.position.x;
-        //    if (transform.position.x < offset)
-        //        x = Mathf.Clamp(transform.position.x, -roomController.MaxDraggableBoundaries.x + padding + offset, -roomController.MinDraggableBoundaries.x - padding + offset);
-        //    else
-        //        x = Mathf.Clamp(transform.position.x, roomController.MinDraggableBoundaries.x + padding + offset, roomController.MaxDraggableBoundaries.x - padding + offset);
+        // Set position
+        transform.position = new Vector3(x, y, z);
 
-        //    // Offset Y
-        //    float minHeight = fruitCollider.bounds.size.y / 2f;
-        //    padding = fruitCollider.bounds.size.y / 4f;
-        //    offset = roomController.Room.transform.position.y;
-        //    float y = Mathf.Clamp(transform.position.y, minHeight + padding + offset, roomController.MaxDraggableBoundaries.y - padding + offset);
-
-        //    // Offset Z
-        //    float z;
-        //    padding = fruitCollider.bounds.size.z / 4f;
-        //    offset = roomController.Room.transform.position.z;
-        //    if (transform.position.z < offset)
-        //        z = Mathf.Clamp(transform.position.z, -roomController.MaxDraggableBoundaries.z + padding + offset, -roomController.MinDraggableBoundaries.z - padding + offset);
-        //    else
-        //        z = Mathf.Clamp(transform.position.z, roomController.MinDraggableBoundaries.z + padding + offset, roomController.MaxDraggableBoundaries.z - padding + offset);
-
-        //    // Set position
-        //    transform.position = new Vector3(x, y, z);
-        //}
     }           
 
     public void Explode()
