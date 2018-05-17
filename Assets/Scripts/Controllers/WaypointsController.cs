a2380e149988933b19f74a56f71d8dd521c2d22c
Jay <jay@vertecx.net>
2018-05-17 15:29:04 -0700

    Waypoint setup

---
+++ Assets/Scripts/Controllers/WaypointsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointsController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] Waypoints;
+
+
+	void Start ()
+    {
+
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnDrawGizmos()
+    {
+        if(Waypoints != null && Waypoints.Length > 0)
+        {
+            Gizmos.color = Color.green;
+
+            foreach (Transform t in Waypoints)
+                Gizmos.DrawSphere(t.position, 0.01f);
+
+            Gizmos.color = Color.red;
+
+            for (int i = 0; i < Waypoints.Length - 1; i++)
+                Gizmos.DrawLine(Waypoints[i].position, Waypoints[i + 1].position);
+        }
+    }
+}
