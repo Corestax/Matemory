ea37a370cf9446dde61f74cfbd7c9f3bc328ed6d
Jay <jay@vertecx.net>
2018-06-05 13:44:37 -0700

    - Added character/spline follower to cake - Added character script to play/stop animation

---
+++ Assets/Character.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dreamteck.Splines;
+
+public class Character : MonoBehaviour
+{
+    private SplineFollower follower;
+
+    void Start ()
+    {
+        follower = GetComponent<SplineFollower>();
+	}
+	
+	void Update ()
+    {
+		
+	}    
+
+    public void SetPosition(float distance)
+    {
+        follower.SetDistance(distance);
+    }
+
+    public void Play()
+    {
+        follower.autoFollow = true;
+    }
+
+    public void Stop()
+    {
+        follower.autoFollow = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Trigger!");
+        Stop();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+}
--- Assets/Scripts/SplineTest.cs
+++ Assets/Scripts/SplineTest.cs
@@ -10,11 +10,29 @@ public class SplineTest : MonoBehaviour
     [SerializeField]
     private SplineFollower follower;
 
+    private Character character;
     private PathGenerator path;
+    private Material[] material;
 
     void Start()
     {
+        character = follower.GetComponent<Character>();
         path = spline.GetComponent<PathGenerator>();
+
+        // TEMPORARY EDITOR FIX FOR SHADER
+        var meshes = follower.GetComponentsInChildren<MeshRenderer>();
+        foreach (var m in meshes)
+        {
+            // Clone material
+            m.material.CopyPropertiesFromMaterial(new Material(m.material));
+
+            // Determine if ARCore lighting should be enabled
+#if UNITY_EDITOR
+            m.material.DisableKeyword("ARCORELIGHT_ON");
+#else
+            m.material.EnableKeyword("ARCORELIGHT_ON");
+#endif
+        }
 
         //follower.computer = computer; //Set the computer of the follower component
         //follower.followSpeed = 5f; //Set the speed of the follower
@@ -48,17 +66,26 @@ public class SplineTest : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            path.SetClipRange(0, 0);
+            character.Play();
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            path.SetClipRange(0, 0.5);
+            character.Stop();
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            path.SetClipRange(0, 1);
-        }
+
+        //if (Input.GetKeyDown(KeyCode.Alpha1))
+        //{
+        //    path.SetClipRange(0, 0);
+        //}
+        //if (Input.GetKeyDown(KeyCode.Alpha2))
+        //{
+        //    path.SetClipRange(0, 0.5);
+        //}
+        //if (Input.GetKeyDown(KeyCode.Alpha3))
+        //{
+        //    path.SetClipRange(0, 1);
+        //}
     }
 }
