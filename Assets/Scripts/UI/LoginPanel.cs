7eae28e54386f34fcafca160c84cf4d4cf7798f5
Yury <yuryherlovich@gmail.com>
2018-06-14 13:05:42 -0700

    Added script for the login panel, added switching login <-> signup

---
+++ Assets/Scripts/UI/LoginPanel.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginPanel : MonoBehaviour
+{
+    [SerializeField]
+    private InputField input_Name;
+    [SerializeField]
+    private InputField input_Email;
+    [SerializeField]
+    private InputField input_Pass;
+    [SerializeField]
+    private Text text_LoginMenu;
+    [SerializeField]
+    private Text text_SignupMenu;
+    [SerializeField]
+    private Color color_ActiveMenu;
+    [SerializeField]
+    private Color color_NotActiveMenu;
+
+    public enum PanelTypes { LOGIN, SIGNUP }
+    private PanelTypes ActivePanel = PanelTypes.LOGIN;
+
+
+    private void OnEnable()
+    {
+        ShowPanel(ActivePanel);
+    }
+
+    public void ShowPanel(PanelTypes panel)
+    {
+        switch (panel)
+        {
+            case PanelTypes.LOGIN:
+                ShowLoginPanel();
+                break;
+            case PanelTypes.SIGNUP:
+                ShowSignupPanel();
+                break;
+            default:
+                break;
+        }
+
+        ActivePanel = panel;
+    }
+
+    public void OnClickShowLoginPanel()
+    {
+        ShowPanel(PanelTypes.LOGIN);
+    }
+
+    public void OnClickShowSignupPanel()
+    {
+        ShowPanel(PanelTypes.SIGNUP);
+    }
+
+    private void ShowLoginPanel()
+    {
+        text_SignupMenu.color = color_NotActiveMenu;
+        text_LoginMenu.color = color_ActiveMenu;
+
+        input_Name.gameObject.SetActive(false);
+    }
+
+    private void ShowSignupPanel()
+    {
+        text_SignupMenu.color = color_ActiveMenu;
+        text_LoginMenu.color = color_NotActiveMenu;
+
+        input_Name.gameObject.SetActive(true);
+    }
+}
