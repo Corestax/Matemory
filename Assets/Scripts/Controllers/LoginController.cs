feadf07b1bfd8b80d26d4c7ba03d4a8f3a291143
Yury <yuryherlovich@gmail.com>
2018-06-14 15:51:09 -0700

    Login/Sign up functions

--- Assets/Scripts/Controllers/LoginController.cs
+++ Assets/Scripts/Controllers/LoginController.cs
@@ -10,17 +10,46 @@ public class LoginController : Singleton<LoginController>
 
     public void Signup(string name, string email, string pass, Action<bool, UserError> callback = null)
     {
-        Debug.Log("sign up");
+        WWWForm form = new WWWForm();
 
-        if (callback != null)
-            callback(false, null);
+        form.AddField("auth_type", (int)DB.UserAuthTypes.SIGNUP);
+        form.AddField("name", name);
+        form.AddField("email", email);
+        form.AddField("password", pass);
+
+        SendRequest(DB.URL_USER, form, callback);
     }
 
     public void Login(string email, string pass, Action<bool, UserError> callback = null)
     {
-        Debug.Log("login");
+        WWWForm form = new WWWForm();
 
-        if (callback != null)
-            callback(false, null);
+        form.AddField("auth_type", (int)DB.UserAuthTypes.LOGIN);
+        form.AddField("email", email);
+        form.AddField("password", pass);
+
+        SendRequest(DB.URL_USER, form, callback);
+    }
+
+    private void SendRequest(string url, WWWForm form, Action<bool, UserError> callback = null)
+    {
+        StartCoroutine(SendRequestCR(url, form, callback));
+    }
+
+    IEnumerator SendRequestCR(string url, WWWForm form, Action<bool, UserError> callback = null)
+    {
+        using (WWW www = new WWW(url, form))
+        {
+            yield return www;
+
+            // save UserData (name)?
+            if (string.IsNullOrEmpty(www.error))
+                Debug.Log(www.text);
+            else
+                Debug.LogWarning(www.text);
+            
+            if (callback != null)
+                callback(false, null);
+        }
     }
 }
